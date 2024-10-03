6b51cc22b9fc9eafcfb9bb09e3f07004fbcda033
Denis Saygin <oreshka.net@gmail.com>
2024-10-03 21:23:03 +0300

    интерфейс работы с key-value

---
+++ keyvaluedb/keyvaluedb.go
@@ -0,0 +1,17 @@
+// Copyright 2024 Kontora13. All rights reserved.
+// Licensed under the Apache License, Version 2.0
+
+// Интерфейс, описывающий взаимодействие с Key-Value хранилищем
+
+package keyvaluedb
+
+import (
+	"context"
+)
+
+// Connection - интерфейс подключения к Key-Value
+type Connection interface {
+	Write(ctx context.Context, key string, value any) error
+	Read(ctx context.Context, key string, value any) error
+	Close() error
+}
