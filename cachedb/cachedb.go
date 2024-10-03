bacd7b528dd0f6fecc7b187fe758d185ada2e231
Denis Saygin <oreshka.net@gmail.com>
2024-10-03 21:22:43 +0300

    cachedb - интерфейс работы с кешом

---
+++ cachedb/cachedb.go
@@ -0,0 +1,18 @@
+// Copyright 2024 Kontora13. All rights reserved.
+// Licensed under the Apache License, Version 2.0
+
+// Интерфейс, описывающий взаимодействие с кэш-хранилищем
+
+package cachedb
+
+import (
+	"context"
+	"time"
+)
+
+// Connection - интерфейс подключения к Key-Value
+type Connection interface {
+	Write(ctx context.Context, key string, value any, ttl time.Duration) error
+	Read(ctx context.Context, key string, value any) error
+	Close() error
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kontora13-go/coreutil
+
+go 1.23.2
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/hashicorp/go-version v1.6.0
+	github.com/jmoiron/sqlx v1.3.5
+)
