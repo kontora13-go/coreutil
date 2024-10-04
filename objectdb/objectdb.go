f40b10462598fefb79ddc646b1c794652c1dbb71
Denis Saygin <oreshka.net@gmail.com>
2024-10-04 06:27:21 +0300

    Черновик объектного хранилища

---
+++ objectdb/objectdb.go
@@ -0,0 +1,11 @@
+// Copyright 2024 Kontora13. All rights reserved.
+// Licensed under the Apache License, Version 2.0
+
+// Описание интерфейса работы с объектным хранилищем
+
+package objectdb
+
+type Connection interface {
+	// TODO: запись, чтение объекта
+	Close() error
+}
